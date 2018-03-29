c17a7eb06e09b060318ad6281cfad600f3314b05
mark4262 <37773875+mark4262@users.noreply.github.com>
2018-03-28 21:16:35 -0400

    Add files via upload

---
+++ VPSci/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VPSci;
+
+
+namespace VPSci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speed = 0;
+            bool bRet = true;
+            string errStr = string.Empty;
+            Shaker ShakerDevice = new Shaker();
+            if (args[0].ToLower() == "start")
+            {
+                if (args.GetUpperBound(0) > 0)
+                {
+                    try
+                    {
+                        speed = Convert.ToInt32(args[1]);
+                        ShakerDevice.Start(speed);
+                    }
+                    catch (Exception)
+                    {
+                        errStr = "Could not convert speed value";
+                    }
+                }
+            }
+            if (args[0].ToLower() == "setport")
+            {
+                if (args.GetUpperBound(0) > 0)
+                {
+                    foreach (string item in args)
+                    {
+                        if (item.ToLower().StartsWith("com"))
+                        {
+                            bRet = ShakerDevice.SetComport(item);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
